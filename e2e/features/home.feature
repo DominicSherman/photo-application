76a3c129f950a7c666c45ea9ec3549d5757e96f3
Dominic <dsherman@hy-vee.com>
2018-11-27 11:23:11 -0600

    E2E testing

--- e2e/features/home.feature
+++ e2e/features/home.feature
@@ -1,2 +1,7 @@
-@LogMeIn
-  Given 
+@home @LogMeIn
+Feature: Test home screen
+
+  Scenario: When I click on the Select Images button I should see the Select Images Modal
+    Given I have logged in
+    When I click on the Select Images button
+    Then I should see a Touchable Image
--- e2e/features/step-definitions/homeSteps.js
+++ e2e/features/step-definitions/homeSteps.js
@@ -1,21 +1,17 @@
-import {email} from '../support/user-info';
+const {Given, When, Then} = require('cucumber');
 
-describe.only('Home', () => {
-    beforeAll(async () => {
-        await login(email);
-    });
+Given(/^I have logged in$/, async () => {
+    await expect(element(by.label('Upload'))).toBeVisible();
+});
 
-    beforeEach(async () => {
-        await device.reloadReactNative();
-    });
+When(/^I click on the Select Images button$/, async () => {
+    await element(by.label('Select Images')).tap();
+});
 
-    it('should open image modal and allow user to select images', async () => {
-        await switchToDev();
+Then(/^I should see a Touchable Image$/, async () => {
+    await expect(element(by.label('Select Images to Upload'))).toBeVisible();
+});
 
-        await element(by.label('Select Images')).tap();
-
-        await expect(element(by.label('Select Images to Upload'))).toBeVisible();
-
-        await element(by.id('touchableImage-IMG_0001.JPG')).tap();
-    });
+When(/^I click on a Touchable Image$/, async () => {
+    await element(by.id('touchableImage-IMG_0001.JPG')).tap();
 });
